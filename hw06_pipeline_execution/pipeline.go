f71902e11e646cab0655f9987b71bd5dcc46b4ef
Viacheslav Trushkin <dogonthesun@gmail.com>
2025-03-19 23:05:21 +0300

    hw06 done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,14 +1,45 @@
 package hw06pipelineexecution
 
 type (
-	In  = <-chan interface{}
+	In  = <-chan any
 	Out = In
-	Bi  = chan interface{}
+	Bi  = chan any
 )
 
 type Stage func(in In) (out Out)
 
-func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+func ExecutePipeline(in In, cancel In, stages ...Stage) Out {
+	wrapper := func(in In, cancel In) Out {
+		out := make(Bi)
+		go func() {
+			defer func() {
+				close(out)
+				for range in { //nolint:all
+				}
+			}()
+			for {
+				select {
+				case <-cancel:
+					return
+				case v, ok := <-in:
+					if !ok {
+						return
+					}
+					select {
+					case out <- v:
+					case <-cancel:
+						return
+					}
+				}
+			}
+		}()
+		return out
+	}
+
+	out := in
+	for _, stage := range stages {
+		out = stage(wrapper(out, cancel))
+	}
+
+	return wrapper(out, cancel)
 }
