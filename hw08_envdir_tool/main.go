0bc73d96958985458b35c23f1a3bea971c9340be
Viacheslav Trushkin <vyacheslav.trushkin@gmail.com>
2025-04-01 17:19:03 +0300

    hw08 done

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+var progname string
+
+func init() {
+	progname = os.Args[0]
+}
+
+func printUsage() {
+	fmt.Printf("%[1]s: usage: %[1]s dir child\n", progname)
+}
+
+const FailExitCode = 111
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		printUsage()
+		os.Exit(FailExitCode)
+	}
+
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		fmt.Printf("%s: %v", progname, err)
+		os.Exit(FailExitCode)
+	}
+
+	os.Exit(RunCmd(os.Args[2:], env))
 }
